cd6496c79604670c289bdcfccafcd8ddfceabba6
Diego <porfiriodiego64@gmail.com>
2024-05-27 13:12:15 -0300

    refactor(test-basic) test rules

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,12 @@
-import { defineAbilityFor } from '@saas/auth'
+import { defineAbilityFor, projectSchema } from '@saas/auth'
 
 const ability = defineAbilityFor({ role: 'MEMBER', id: '1' })
 
+const project = projectSchema.parse({ id: '1', ownerId: '1' })
+
 const userCanInviteSomeoneElse = ability.can('invite', 'User')
 const userCanDeleteOtherUsers = ability.can('delete', 'User')
-const userCanDeleteProjects = ability.can('delete', 'Project')
+const userCanDeleteProjects = ability.can('delete', project)
 
 console.log(userCanInviteSomeoneElse)
 console.log(userCanDeleteOtherUsers)
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -29,6 +29,10 @@ export type AppAbility = MongoAbility<AppAbilities>
 
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
 
+export * from './models/organization'
+export * from './models/project'
+export * from './models/user'
+
 export function defineAbilityFor(user: User) {
   const builder = new AbilityBuilder(createAppAbility)
 
