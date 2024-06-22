0303c24209f885ff35b3b4aef3d513b23b07c70b
Diego <porfiriodiego64@gmail.com>
2024-06-22 20:03:19 -0300

    feat(api: get-pending-invites) route

---
+++ apps/api/src/http/routes/invites/get-pending-invites.ts
@@ -0,0 +1,100 @@
+import { roleSchema } from '@saas/auth'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function getPendingInvites(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/pending-invites',
+      {
+        schema: {
+          tags: ['invites'],
+          summary: 'Get all user pending invites',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.array(
+              z.object({
+                invites: z.object({
+                  organization: z.object({
+                    name: z.string(),
+                  }),
+                  author: z
+                    .object({
+                      id: z.string().uuid(),
+                      name: z.string().nullable(),
+                      avatarUrl: z.string().url().nullable(),
+                    })
+                    .nullable(),
+                  email: z.string().email(),
+                  role: roleSchema,
+                }),
+              }),
+            ).nullable,
+          },
+        },
+      },
+      async (request) => {
+        const { slug } = request.params
+
+        const { membership } = await request.getUserMembership(slug)
+
+        const userId = await request.getCurrentUserId()
+        const user = await prisma.user.findUnique({
+          where: {
+            id: userId,
+          },
+        })
+
+        if (!user) {
+          throw new BadRequestError('User not found !')
+        }
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('get', 'Invite')) {
+          throw new UnauthorizedError(
+            `You're not allowed to get organization invites.`,
+          )
+        }
+
+        const invites = await prisma.invite.findMany({
+          where: {
+            email: user.email,
+          },
+          select: {
+            id: true,
+            email: true,
+            role: true,
+            createdAt: true,
+            author: {
+              select: {
+                id: true,
+                name: true,
+                avatarUrl: true,
+              },
+            },
+            organization: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        })
+
+        return { invites }
+      },
+    )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -22,6 +22,7 @@ import { acceptInvite } from './routes/invites/accept-invite'
 import { createInvite } from './routes/invites/create-invite'
 import { getInvite } from './routes/invites/get-invite'
 import { getInvites } from './routes/invites/get-invites'
+import { getPendingInvites } from './routes/invites/get-pending-invites'
 import { rejectInvite } from './routes/invites/reject-invite'
 import { deleteMember } from './routes/members/delete-members'
 import { getMembers } from './routes/members/get-members'
@@ -113,6 +114,7 @@ app.register(getInvites)
 app.register(acceptInvite)
 app.register(rejectInvite)
 app.register(rejectInvite)
+app.register(getPendingInvites)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('Server is running on port 3333')
