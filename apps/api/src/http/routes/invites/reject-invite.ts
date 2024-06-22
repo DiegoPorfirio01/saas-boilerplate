165f130d050ab17f23a8173696f59a660d1d1ddf
Diego <porfiriodiego64@gmail.com>
2024-06-22 19:24:22 -0300

    feat(api: reject-invite) route

---
+++ apps/api/src/http/routes/invites/reject-invite.ts
@@ -0,0 +1,67 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function rejectInvite(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .delete(
+      '/invites/:inviteId/reject',
+      {
+        schema: {
+          tags: ['invites'],
+          summary: 'reject an invite',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            inviteId: z.string().uuid(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { inviteId } = request.params
+
+        const invite = await prisma.invite.findUnique({
+          where: {
+            id: inviteId,
+          },
+        })
+
+        if (!invite) {
+          throw new BadRequestError('This invite does not exist')
+        }
+
+        const userId = await request.getCurrentUserId()
+
+        const user = await prisma.user.findUnique({
+          where: {
+            id: userId,
+          },
+        })
+
+        if (!user) {
+          throw new BadRequestError('User not found')
+        }
+
+        if (invite.email !== user.email) {
+          throw new BadRequestError('This invite belogs to another user')
+        }
+
+        await prisma.invite.delete({
+          where: {
+            id: inviteId,
+          },
+        })
+
+        return reply.status(204).send()
+      },
+    )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -22,6 +22,7 @@ import { acceptInvite } from './routes/invites/accept-invite'
 import { createInvite } from './routes/invites/create-invite'
 import { getInvite } from './routes/invites/get-invite'
 import { getInvites } from './routes/invites/get-invites'
+import { rejectInvite } from './routes/invites/reject-invite'
 import { deleteMember } from './routes/members/delete-members'
 import { getMembers } from './routes/members/get-members'
 import { updateMember } from './routes/members/update-members'
@@ -110,6 +111,7 @@ app.register(createInvite)
 app.register(getInvite)
 app.register(getInvites)
 app.register(acceptInvite)
+app.register(rejectInvite)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('Server is running on port 3333')
