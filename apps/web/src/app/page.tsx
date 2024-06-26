7dd058b5ec8aa5131b0e1ee2ba0dbcd2a42c30d2
Diego <porfiriodiego64@gmail.com>
2024-06-26 11:17:15 -0300

    feat(front: init next 15)

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 sm:p-20">
+      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
+        Hello world
+      </main>
+      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6"></footer>
+    </div>
+  )
+}
