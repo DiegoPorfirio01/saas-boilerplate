7fed2e204fff8be1bbef73e8de4b271764025c70
Diego <diego.vianna@rua.com.br>
2024-09-15 23:33:59 -0300

    feat(web: hook form state ) implements reset form after submit

--- apps/web/src/hooks/useFormState.ts
+++ apps/web/src/hooks/useFormState.ts
@@ -1,6 +1,7 @@
 'use client'
 
 import { type FormEvent, useState, useTransition } from 'react'
+import { requestFormReset } from 'react-dom'
 
 interface FormState {
   success: boolean
@@ -38,6 +39,8 @@ export function useFormState(
 
       setFormState(state)
     })
+
+    requestFormReset(form)
   }
 
   return [formState, handleSubmit, isPending] as const
