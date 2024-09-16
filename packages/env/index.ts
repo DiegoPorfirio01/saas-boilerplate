95585e8ea8c7c30a161b9172294e08872b070ae3
Diego <diego.vianna@rua.com.br>
2024-09-16 11:30:03 -0300

    fix(env) enviroment variables

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -3,27 +3,28 @@ import { z } from 'zod'
 
 export const env = createEnv({
   server: {
-    SERVER_PORT: z.coerce.number().default(3333),
+    PORT: z.coerce.number().default(3333),
+    DATABASE_URL: z.string().url(),
+
     JWT_SECRET: z.string(),
 
-    DATABASE_URL: z.string().url(),
     GITHUB_OAUTH_CLIENT_ID: z.string(),
     GITHUB_OAUTH_CLIENT_SECRET: z.string(),
     GITHUB_OAUTH_CLIENT_REDIRECT_URI: z.string().url(),
   },
   client: {},
   shared: {
-    PORT: z.string().url(),
+    NEXT_PUBLIC_API_URL: z.string().url(),
   },
   runtimeEnv: {
-    SERVER_PORT: process.env.SERVER_PORT,
-    JWT_SECRET: process.env.JWT_SECRET,
+    PORT: process.env.SERVER_PORT,
     DATABASE_URL: process.env.DATABASE_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
     GITHUB_OAUTH_CLIENT_ID: process.env.GITHUB_OAUTH_CLIENT_ID,
     GITHUB_OAUTH_CLIENT_SECRET: process.env.GITHUB_OAUTH_CLIENT_SECRET,
     GITHUB_OAUTH_CLIENT_REDIRECT_URI:
       process.env.GITHUB_OAUTH_CLIENT_REDIRECT_URI,
-    PORT: process.env.PORT,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
   emptyStringAsUndefined: true,
 })
