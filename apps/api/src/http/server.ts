fde9353fdb36e8c1f1b8bc0a5086f76ecf829a40
Diego <diego.vianna@rua.com.br>
2024-09-16 11:34:33 -0300

    fix(env) enviroment variables

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -120,6 +120,6 @@ app.register(getPendingInvites)
 
 app.register(getOrganizationBilling)
 
-app.listen({ port: env.SERVER_PORT }).then(() => {
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('Server is running on port 3333')
 })
